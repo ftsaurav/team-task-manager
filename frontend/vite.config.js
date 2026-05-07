680cce7d0e9f5c757be04f3d37327c667621c58e
Saurav Kumar <ft.saurav17@gmail.com>
2026-05-07 05:56:24 +0530

    railway.json added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+
+  plugins: [react()],
+
+  preview: {
+
+    host: '0.0.0.0',
+
+    port: process.env.PORT || 8080,
+
+    allowedHosts: true
+  }
 })
