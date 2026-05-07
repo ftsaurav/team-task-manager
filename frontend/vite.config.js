e73771fe04db3596bc5732ab73c7f14e81a9b862
Saurav Kumar <ft.saurav17@gmail.com>
2026-05-07 06:35:52 +0530

    fixed vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    host: '0.0.0.0'
-  },
-
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 8080,
-    allowedHosts: true
-  }
+  base: '/'
 })
