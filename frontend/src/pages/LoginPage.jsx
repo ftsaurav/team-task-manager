26efdf2d96a17affd0a9d8fe0763dabcd4811b70
Saurav Kumar <ft.saurav17@gmail.com>
2026-05-07 08:23:24 +0530

    talwind removed

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,53 +1,24 @@
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import './Login.css'
+export default function LoginPage() {
 
-import { loginUser } from "../api/authApi";
-import { AuthContext } from "../context/AuthContext";
-import { toast } from "react-toastify";
-
-function LoginPage() {
-  const navigate = useNavigate();
-
-  const { login } = useContext(AuthContext);
-
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await loginUser(formData);
-
-      login(response.token);
-      toast.success("Login Successful");
-
-      navigate("/dashboard");
-    } catch (err) {
-      toast.error(
-    err.response?.data?.message ||
-    "Login Failed"
-);
-    }
-  };
-  
   return (
-  <div className="min-h-screen bg-black flex items-center justify-center">
-    <div className="bg-red-500 text-white p-10 text-5xl rounded-xl">
-      Tailwind Working
-    </div>
-  </div>
-)
-}
 
-export default LoginPage;
+    <div className="login-container">
+
+      <div className="login-card">
+
+        <h1>Team Task Manager</h1>
+
+        <p>Login to continue</p>
+
+        <input type="email" placeholder="Email" />
+
+        <input type="password" placeholder="Password" />
+
+        <button>Login</button>
+
+      </div>
+
+    </div>
+  )
+}
